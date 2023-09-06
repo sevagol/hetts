777374198cad12ec1240cc50e04f4a35c5f94b3c
sevagol <sevgol1553@gmail.com>
2023-09-06 19:27:44 +0200

    f

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import csv
+from urllib.parse import urlparse, parse_qs
 
 app = Flask(__name__)
 
@@ -19,23 +20,31 @@ def index():
         return render_template('index.html', results=results, search_query=search_query)
     return render_template('index.html', results=None, search_query=None)
 
-# Функция для поиска по CSV
-# Функция для поиска по CSV и удаления дубликатов
+# Функция для поиска по CSV и формирования текста для ссылок
+# Функция для поиска по CSV и формирования текста для ссылок
 def search_in_csv(query):
     results = []
-    seen = set()  # Множество для отслеживания уникальных результатов
+    seen = set()
     for row in data:
         url, paragraph, paragraph_text = row
-        url = url.strip("[]")
-        url = url.strip("'")
-        paragraph = paragraph.strip("[]")
-        paragraph = paragraph.strip("'")
+        url = paragraph.strip("[]")
+        url = paragraph.strip("'")
         if query in paragraph_text:
-            result_tuple = (url, paragraph)  # Создаем кортеж (URL, параграф)
-            if result_tuple not in seen:  # Проверяем, не является ли кортеж дубликатом
+            result_tuple = (url, paragraph)
+            if result_tuple not in seen:
                 seen.add(result_tuple)
-                results.append({'url': url, 'paragraph': paragraph})
+                # Извлекаем значения xst и prgr из URL
+                parsed_url = urlparse(url)
+                xst = parse_qs(parsed_url.query).get('xst', [''])[0]
+                prgr = parse_qs(parsed_url.query).get('prgr', [''])[0]
+                # Формируем текст для ссылки с использованием HTML-сущностей
+                link_text = f"{xst}. &sect;{prgr}"
+                # Сохраняем URL без изменений
+                results.append({'url': url, 'paragraph': link_text})
     return results
+
+
+
 
 
 # Отключите авто-экранирование URL в шаблоне
